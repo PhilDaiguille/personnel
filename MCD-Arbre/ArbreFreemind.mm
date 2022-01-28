39bb45c47568a1c99159395efe10686fa77c67a9
PhilDaiguille <80407535+PhilDaiguille@users.noreply.github.com>
2022-01-28 10:31:18 +0100

    Arbre Heuristique rajout

---
+++ MCD-Arbre/ArbreFreemind.mm
@@ -0,0 +1,100 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1643359840727" ID="4ap56i6hprmea6e90atun2g071" MODIFIED="1643359840727" STYLE="bubble" TEXT="ACCUEIL">
+<node COLOR="#030303" CREATED="1643359840727" ID="2qe7k34a3hv52cn44gi7f9s678" MODIFIED="1643359840727" POSITION="right" STYLE="bubble" TEXT="G&#xe9;rer le compte root">
+<edge WIDTH="8"/>
+<node CREATED="1643359840727" ID="19apm2bhfch8ntsub8jp76ufgh" MODIFIED="1643359840727" STYLE="bubble" TEXT="Afficher Employ&#xe9;s">
+<edge COLOR="#c0504d" STYLE="bezier" WIDTH="8"/>
+</node>
+<node CREATED="1643359840727" ID="7hpe0nc932njduid3lmd912rr9" MODIFIED="1643359840727" TEXT="Changer le nom">
+<edge COLOR="#c0504d" STYLE="bezier" WIDTH="8"/>
+</node>
+<node CREATED="1643359840727" ID="5hbkm2h56ufhjnfi7qtml2s86r" MODIFIED="1643359840727" STYLE="bubble" TEXT="Changer le pr&#xe9;nom">
+<edge COLOR="#c0504d" STYLE="bezier" WIDTH="8"/>
+</node>
+<node CREATED="1643359840727" ID="5vd718rmrfb8n8e97fm1otif1i" MODIFIED="1643359840727" TEXT="Changer le mail">
+<edge COLOR="#c0504d" STYLE="bezier" WIDTH="8"/>
+</node>
+<node CREATED="1643359840727" ID="3mfkn5fhgvlb1cba43v7pp54lo" MODIFIED="1643359840727" STYLE="bubble" TEXT="Changer le password">
+<edge COLOR="#c0504d" STYLE="bezier" WIDTH="8"/>
+</node>
+<node CREATED="1643359840727" ID="4t156cqtlk5q92qhte9760lilu" MODIFIED="1643359840727" STYLE="bubble" TEXT="Back">
+<edge COLOR="#c0504d" STYLE="bezier" WIDTH="8"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#f2f2f2" COLOR="#f2f2f2" CREATED="1643359840727" ID="7krh0rplvln9khrmbkiogc6fk2" MODIFIED="1643359840727" POSITION="right" TEXT="Sujet flottant">
+<edge WIDTH="8"/>
+<font NAME="SansSerif" SIZE="8"/>
+</node>
+<node CREATED="1643359840727" ID="1euin1i6t4faqnf71flr7pe7q3" MODIFIED="1643359840727" POSITION="right" STYLE="bubble" TEXT="G&#xe9;rer les ligues">
+<edge WIDTH="8"/>
+<node CREATED="1643359840727" ID="0sh8fpa5bekuh7gef9ab35i0fb" MODIFIED="1643359840727" STYLE="bubble" TEXT="Afficher les ligues">
+<edge COLOR="#9bbb59" WIDTH="8"/>
+</node>
+<node CREATED="1643359840727" ID="7jrlnn3dclhpo4dthl9800dhtt" MODIFIED="1643359840727" STYLE="bubble" TEXT="Ajouter une ligue">
+<edge COLOR="#9bbb59" WIDTH="8"/>
+<node CREATED="1643359840727" ID="5av0i91n8vlivnd50ajo5j2l2t" MODIFIED="1643359840727" TEXT="Nom"/>
+</node>
+<node CREATED="1643359840727" ID="3cs7t4hudca3naq0jbosjgcmnc" MODIFIED="1643359840727" STYLE="bubble" TEXT="S&#xe9;lectionner une ligue">
+<edge COLOR="#9bbb59" WIDTH="8"/>
+</node>
+<node CREATED="1643359840727" ID="5850b9aaf1ntbacqqtvets35r8" MODIFIED="1643359840727" STYLE="bubble" TEXT="Back">
+<edge COLOR="#9bbb59" WIDTH="8"/>
+<node CREATED="1643359840727" ID="31i48ii77u2m1497fk8v5buqu7" MODIFIED="1643359840727" TEXT="Editer [Nom de la ligue]">
+<node CREATED="1643359840727" ID="3n5294f4ma6laovrakvcf6225j" MODIFIED="1643359840727" TEXT="Afficher la ligue" VSHIFT="395"/>
+<node CREATED="1643359840727" ID="761jm8fs784arjb0eloesmoge0" MODIFIED="1643359840727" TEXT="G&#xe9;rer les employ&#xe9;s de [Nom de la ligue]" VSHIFT="302">
+<node CREATED="1643359840727" ID="b8c92a3c-e66a-4d62-965d-ca83d944feef" MODIFIED="1643359840727" STYLE="bubble" TEXT="Afficher les employ&#xe9;s" VSHIFT="215"/>
+<node CREATED="1643359840727" ID="dc96748c-fbf9-451f-ba86-150d886b15c2" MODIFIED="1643359840727" TEXT="Ajouter un employ&#xe9;" VSHIFT="215">
+<node CREATED="1643359840727" ID="dc6c67c4-3103-4cd4-b505-7e610c8c711a" MODIFIED="1643359840727" TEXT="Nom" VSHIFT="215">
+<node CREATED="1643359840727" ID="d06ec0ce-4d85-4b97-998f-3347c4f4c502" MODIFIED="1643359840727" TEXT="Pr&#xe9;nom" VSHIFT="212">
+<node CREATED="1643359840727" ID="ec2e05d0-6cae-4dcd-9fb6-27233c10a98c" MODIFIED="1643359840727" TEXT="Mail" VSHIFT="212">
+<node CREATED="1643359840727" ID="d499efa5-8afe-43dc-9069-e6305df67610" MODIFIED="1643359840727" TEXT="Password" VSHIFT="212"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1643359840727" ID="981f3fb0-45f2-4da6-9af4-0406b604c2d9" MODIFIED="1643359840727" TEXT="Modifier un employ&#xe9;" VSHIFT="215">
+<node CREATED="1643359840727" ID="fb4108c6-e957-44cb-a341-f4b8914309d0" MODIFIED="1643359840727" TEXT="G&#xe9;rer le compte [Nom de l&apos;employ&#xe9;]" VSHIFT="212">
+<node CREATED="1643359840727" ID="70cb4ef2-5d14-40c6-bbf4-38d7a192abe3" MODIFIED="1643359840727" TEXT="Afficher l&apos;employ&#xe9;" VSHIFT="212"/>
+<node CREATED="1643359840727" ID="68421dd1-eff3-4d49-ad7e-d47a949937c7" MODIFIED="1643359840727" STYLE="bubble" TEXT="Changer le nom" VSHIFT="212">
+<node CREATED="1643359840727" ID="3aad1a92-4725-41bd-9819-1b62ebb732c0" MODIFIED="1643359840727" TEXT="Nouveau nom : [ins&#xe9;rez un nom]" VSHIFT="366"/>
+</node>
+<node CREATED="1643359840727" ID="8d30980c-6622-4cfa-b074-c892e86ccf09" MODIFIED="1643359840727" TEXT="Changer le pr&#xe9;nom" VSHIFT="212">
+<node CREATED="1643359840727" ID="6f055f7b-2c70-4d2e-8e26-ff5f31a71abc" MODIFIED="1643359840727" TEXT="Nouveau pr&#xe9;nom : [ins&#xe9;rez un pr&#xe9;nom]" VSHIFT="366"/>
+</node>
+<node CREATED="1643359840727" ID="33ccbed8-cb76-4acc-8f87-c6e8c5392fc4" MODIFIED="1643359840727" TEXT="Changer le mail" VSHIFT="212">
+<node CREATED="1643359840727" ID="d0b29ceb-1a1a-4b7c-9f6b-9899031c6a57" MODIFIED="1643359840727" TEXT="Nouveau mail : [ins&#xe9;rez un mail]" VSHIFT="364"/>
+</node>
+<node CREATED="1643359840727" ID="ddf48bef-8092-432e-8af8-fe91ca8d4d8c" MODIFIED="1643359840727" TEXT="Changer le password" VSHIFT="212">
+<node CREATED="1643359840727" ID="19df69c2-1284-41ec-a804-7b4ef18f16a1" MODIFIED="1643359840727" TEXT="Nouveau password : [ins&#xe9;rez un password]" VSHIFT="363"/>
+</node>
+<node CREATED="1643359840727" ID="db9da08d-7c57-4586-9a68-9e7ceaa44de2" MODIFIED="1643359840727" TEXT="Back" VSHIFT="212"/>
+</node>
+</node>
+<node CREATED="1643359840727" ID="d9892e58-4ddf-470b-b48c-cc3259cf1569" MODIFIED="1643359840727" TEXT="Supprimer un employ&#xe9;" VSHIFT="215"/>
+<node CREATED="1643359840727" ID="18956eb5-a1b5-4fa3-bb35-2a931d25aec2" MODIFIED="1643359840727" TEXT="Back" VSHIFT="215"/>
+</node>
+<node CREATED="1643359840727" ID="7uc6q0uq2f6njcd5lgb30bra12" MODIFIED="1643359840727" STYLE="bubble" TEXT="Renommer" VSHIFT="351"/>
+<node CREATED="1643359840727" ID="7nd6dsdnnamvad3tvu6bni2t9h" MODIFIED="1643359840727" TEXT="Supprimer" VSHIFT="326"/>
+<node CREATED="1643359840727" ID="782olem1msdfprbhj4kiaauccu" MODIFIED="1643359840727" TEXT="Back" VSHIFT="305"/>
+</node>
+</node>
+</node>
+<node BACKGROUND_COLOR="#f2f2f2" COLOR="#f2f2f2" CREATED="1643359840727" ID="e9d58bbb-9c9b-496c-8c2a-ee47a341a6b9" MODIFIED="1643359840727" POSITION="left" TEXT="Sujet flottant" VSHIFT="260">
+<edge WIDTH="8"/>
+<font NAME="SansSerif" SIZE="8"/>
+</node>
+<node BACKGROUND_COLOR="#ffffff" COLOR="#030303" CREATED="1643359840727" ID="886e0676-da0c-4156-9e23-af577d0194a0" MODIFIED="1643359840727" POSITION="left" STYLE="bubble" TEXT="Quitter" VSHIFT="143">
+<edge WIDTH="8"/>
+<node COLOR="#030303" CREATED="1643359840727" ID="724f7b86-c3dd-4d03-b60a-696cfef1b881" MODIFIED="1643359840727" STYLE="bubble" TEXT="Quitter et enregistrer">
+<edge COLOR="#558ed5" WIDTH="8"/>
+</node>
+<node COLOR="#030303" CREATED="1643359840727" ID="2b832852-285e-415b-a13c-5101b8e1925c" MODIFIED="1643359840727" STYLE="bubble" TEXT="Quitter sans enregistrer">
+<edge COLOR="#558ed5" WIDTH="8"/>
+</node>
+<node COLOR="#030303" CREATED="1643359840727" ID="58999143-0dd4-46ab-8f62-9d488a052102" MODIFIED="1643359840727" STYLE="bubble" TEXT="Back">
+<edge COLOR="#558ed5" WIDTH="8"/>
+</node>
+</node>
+</node>
+</map>
