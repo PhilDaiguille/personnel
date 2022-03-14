aedfe1520f0620dde1a3414929edb71da8411614
PhilDaiguille <philippe.delente@gmail.com>
2022-03-14 17:15:49 +0100

    Fix Credentials + Fix SQL
    Varchar --> Int

--- Documentation/SQL_PPE.sql
+++ Documentation/SQL_PPE.sql
@@ -1,13 +1,13 @@
 CREATE TABLE `employe` (
-  `id_employee` int(255)  AUTO_INCREMENT,
+  `id_employee` INT(255)  AUTO_INCREMENT,
   `nom` varchar(42) DEFAULT NULL,
   `prenom` varchar(42) DEFAULT NULL,
   `mail` varchar(42) DEFAULT NULL,
   `password` varchar(42) DEFAULT NULL,
   `date_arrivee` DATE,
   `date_depart` DATE,
-  `habilitation` varchar(42) DEFAULT NULL,
-  `id_ligue` int(255) DEFAULT NULL,
+  `habilitation` INT(32),
+  `id_ligue` INT(255) DEFAULT NULL,
   PRIMARY KEY (`id_employee`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
