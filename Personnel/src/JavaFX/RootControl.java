49b6363c37f6b01b29867bf7671e2540c6700c3b
Tinou95 <Quentin95170@hotmail.fr>
2022-04-12 16:44:27 +0200

    fix

--- Personnel/src/JavaFX/RootControl.java
+++ Personnel/src/JavaFX/RootControl.java
@@ -44,7 +44,7 @@ public class RootControl {
 	@FXML
 	private void BackButton() throws Exception {		
 			Parent root;
-			root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+			root = FXMLLoader.load(getClass().getResource("Accueil.fxml"));
 			Stage window = (Stage) back.getScene().getWindow();
 			window.setScene(new Scene(root, 800, 600));
 			
